67814d268c491869d004182678da13ae4bb8c570
h-daria <dariahalaida@gmail.com>
2023-03-19 21:45:22 +0200

    add data to dialog component

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { PlanetData } from '../interfaces/planet-data';
+import { ResidentData } from '../interfaces/resident-data';
+import { PlanetsDataService } from '../services/planets-data.service';
+
+export interface DialogData {
+  url: string;
+}
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
-  styleUrls: ['./dialog.component.scss']
+  styleUrls: ['./dialog.component.scss'],
 })
 export class DialogComponent implements OnInit {
+  public planetData!: PlanetData;
+  public residents: [] = [];
+  public residentsData: ResidentData[] = [];
+  public dataSource: MatTableDataSource<ResidentData> =
+    new MatTableDataSource<ResidentData>();
+  public columns = [
+    {
+      columnDef: 'name',
+      header: 'Name',
+      cell: (resident: ResidentData) => `${resident.name}`,
+    },
+    {
+      columnDef: 'height',
+      header: 'Height',
+      cell: (resident: ResidentData) => `${resident.height}`,
+    },
+    {
+      columnDef: 'birth_year',
+      header: 'Birth Year',
+      cell: (resident: ResidentData) => `${resident.birth_year}`,
+    },
+  ];
+  public displayedColumns = this.columns.map((c) => c.columnDef);
 
-  constructor() { }
+  constructor(
+    private planetsDataService: PlanetsDataService,
+    public dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
 
   ngOnInit(): void {
+    this.getPlanetResidents();
   }
 
+  getPlanetResidents(): void {
+    this.planetsDataService.getPlanetDataById(this.data).subscribe((data) => {
+      this.residents = data.residents;
+      this.residents.forEach((resident) => {
+        this.planetsDataService.getResidentData(resident)
+        .subscribe((data) => {
+          this.residentsData.push(data);
+          if(this.residentsData.length === this.residents.length) {
+            this.dataSource = new MatTableDataSource(this.residentsData);
+          }
+        });
+      });
+    });
+  }
 }
--- src/app/dialog/dialog.module.ts
+++ src/app/dialog/dialog.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DialogComponent } from './dialog.component';
 import { DialogRoutingModule } from './dialog-routing.module';
-
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    DialogComponent
-  ],
-  imports: [
-    CommonModule,
-    DialogRoutingModule
-  ]
+  declarations: [DialogComponent],
+  imports: [CommonModule, DialogRoutingModule, SharedModule],
 })
-export class DialogModule { }
+export class DialogModule {}
