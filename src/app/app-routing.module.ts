41a5c3c3af2cb361ce119932e0230dc8e7e59179
h-daria <dariahalaida@gmail.com>
2023-03-19 19:51:29 +0200

    add project routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'planets-info',
+    pathMatch: 'full'
+  },
+  {
+    path: 'planets-info',
+    loadChildren: () => 
+      import('../app/table/table.module')
+      .then(m => m.TableModule)
+  },
+  {
+    path: 'resident-details',
+    loadChildren: () => 
+      import('../app/dialog/dialog.module')
+      .then(m => m.DialogModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
